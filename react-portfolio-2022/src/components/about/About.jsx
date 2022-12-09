a6e9788a8f49f854737946f5523c9a8d58f77b62
chanwoo Hwang <chanwoo1017@hotmail.com>
2022-12-09 02:18:09 -0800

    change pic size

--- react-portfolio-2022/src/components/about/About.jsx
+++ react-portfolio-2022/src/components/about/About.jsx
@@ -8,7 +8,6 @@ import { VscProject } from 'react-icons/vsc'
 const about = () => {
   return (
     <section id='about'>
-      <h5>Get To Know</h5>
       <h2>About Me</h2>
       <div className='container about_container'>
         <div className='about_me'>
